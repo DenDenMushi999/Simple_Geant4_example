5928b21b45a7e606af390d98c25ee2f0bd0cc532
DenDenMushi999 <38475603+DenDenMushi999@users.noreply.github.com>
2019-10-20 15:07:28 +0300

    Update Simple_Geant4_example.cc

--- Simple_Geant4_example.cc
+++ Simple_Geant4_example.cc
@@ -1,7 +1,7 @@
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
 #ifdef G4UI_USE
-#include "G4VisExecutive.hh"
+    #include "G4VisExecutive.hh"
 #endif
 #include "ExG4DetectorConstruction01.hh"
 #include "ExG4PhysicsList00.hh"
@@ -14,27 +14,32 @@ int main()
     // set mandatory initialization classes
     runManager->SetUserInitialization(new ExG4DetectorConstruction01);
     runManager->SetUserInitialization(new ExG4PhysicsList00);
+    
     // set mandatory user action class
     runManager->SetUserAction(new ExG4PrimaryGeneratorAction01);
+    
     // initialize G4 kernel
     runManager->Initialize();
+    
     // Get the pointer to the User Interface manager
     G4UImanager* UImanager = G4UImanager::GetUIpointer();
+    
     if ( argc == 1 ) {
     // interactive mode : define UI session
     #ifdef G4UI_USE
-    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-    UImanager->ApplyCommand("/control/execute init.mac");
-    ui->SessionStart();
-    delete ui;
+        G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+        UImanager->ApplyCommand("/control/execute init.mac");
+        ui->SessionStart();
+        delete ui;
     #endif
     }
     else {
-    // batch mode
-    G4String command = "/control/execute ";
-    G4String fileName = argv[1];
-    UImanager->ApplyCommand(command+fileName);
+        // batch mode
+        G4String command = "/control/execute ";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command+fileName);
     }
+    
     // job termination
     delete runManager;
     return 0;
