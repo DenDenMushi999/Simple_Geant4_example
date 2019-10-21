c900361ec0df60af6f76383cf315f9f85791a274
student <student@gammacd12.mephi.ru>
2019-10-21 18:07:15 +0300

    create build into project

---
+++ build/CMakeFiles/ExampleG41.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/student/Documents/Davletshin/Simple_Geant4_example/ExG4.cc" "/home/student/Documents/Davletshin/Simple_Geant4_example/build/CMakeFiles/ExampleG41.dir/ExG4.cc.o"
+  "/home/student/Documents/Davletshin/Simple_Geant4_example/source/ExG4ActionInitialization01.cc" "/home/student/Documents/Davletshin/Simple_Geant4_example/build/CMakeFiles/ExampleG41.dir/source/ExG4ActionInitialization01.cc.o"
+  "/home/student/Documents/Davletshin/Simple_Geant4_example/source/ExG4DetectorConstruction01.cc" "/home/student/Documents/Davletshin/Simple_Geant4_example/build/CMakeFiles/ExampleG41.dir/source/ExG4DetectorConstruction01.cc.o"
+  "/home/student/Documents/Davletshin/Simple_Geant4_example/source/ExG4EventAction01.cc" "/home/student/Documents/Davletshin/Simple_Geant4_example/build/CMakeFiles/ExampleG41.dir/source/ExG4EventAction01.cc.o"
+  "/home/student/Documents/Davletshin/Simple_Geant4_example/source/ExG4PrimaryGeneratorAction01.cc" "/home/student/Documents/Davletshin/Simple_Geant4_example/build/CMakeFiles/ExampleG41.dir/source/ExG4PrimaryGeneratorAction01.cc.o"
+  "/home/student/Documents/Davletshin/Simple_Geant4_example/source/ExG4RunAction01.cc" "/home/student/Documents/Davletshin/Simple_Geant4_example/build/CMakeFiles/ExampleG41.dir/source/ExG4RunAction01.cc.o"
+  "/home/student/Documents/Davletshin/Simple_Geant4_example/source/ExG4SteppingAction01.cc" "/home/student/Documents/Davletshin/Simple_Geant4_example/build/CMakeFiles/ExampleG41.dir/source/ExG4SteppingAction01.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OI"
+  "G4VIS_USE_OIX"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/ausr/local/CLHEP/include"
+  "/ausr/local/geant4.10.03.p03/include/Geant4"
+  "../include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/./mkspecs/linux-g++"
+  "/usr/include/qt5/QtPrintSupport"
+  "/usr/include/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ExampleG41.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExampleG41.dir/ExG4.cc.o"
+  "CMakeFiles/ExampleG41.dir/source/ExG4ActionInitialization01.cc.o"
+  "CMakeFiles/ExampleG41.dir/source/ExG4DetectorConstruction01.cc.o"
+  "CMakeFiles/ExampleG41.dir/source/ExG4EventAction01.cc.o"
+  "CMakeFiles/ExampleG41.dir/source/ExG4PrimaryGeneratorAction01.cc.o"
+  "CMakeFiles/ExampleG41.dir/source/ExG4RunAction01.cc.o"
+  "CMakeFiles/ExampleG41.dir/source/ExG4SteppingAction01.cc.o"
+  "ExampleG41.pdb"
+  "ExampleG41"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExampleG41.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
