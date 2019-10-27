efe849f3670a481bdbb1127725a7fc6ddf7e3c39
DenDenMushi <den.davletshin00@mail.ru>
2019-10-27 20:39:41 +0300

    Update ExG4.cc

--- ExG4.cc
+++ ExG4.cc
@@ -17,11 +17,17 @@
 
 int main(int argc,char** argv)
 {
-    
+    // Detect interactive mode (if no arguments) and define UI session
+    //
+    G4UIExecutive* ui = 0;
+    if ( argc == 1 ) {
+        ui = new G4UIExecutive(argc, argv);
+    }
 
     // Choose the Random engine
     G4Random::setTheEngine(new CLHEP::RanecuEngine);
     // construct the default run manager
+    
     G4RunManager* runManager = new G4RunManager;
     
     // set mandatory initialization classes
@@ -35,30 +41,34 @@ int main(int argc,char** argv)
     // set mandatory user action class
     runManager->SetUserInitialization(new ExG4ActionInitialization01());
     
-
     // initialize G4 kernel
     runManager->Initialize();
+    
+    // Initialize visualization
+    //
+    G4VisManager* visManager = new G4VisExecutive;
+    // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+    // G4VisManager* visManager = new G4VisExecutive("Quiet");
+    visManager->Initialize();
     
     // Get the pointer to the User Interface manager
     G4UImanager* UImanager = G4UImanager::GetUIpointer();
     
-    if ( argc == 1 ) {
-    // interactive mode : define UI session
-    #ifdef G4UI_USE
-        G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-        UImanager->ApplyCommand("/control/execute init.mac");
-        ui->SessionStart();
-        delete ui;
-    #endif
-    }
-    else {
+    if ( ! ui ) { 
         // batch mode
         G4String command = "/control/execute ";
         G4String fileName = argv[1];
         UImanager->ApplyCommand(command+fileName);
     }
-    
+    else { 
+        // interactive mode
+        UImanager->ApplyCommand("/control/execute init_vis.mac");
+        ui->SessionStart();
+        delete ui;
+    }
+
     // job termination
+    delete visManager;
     delete runManager;
     return 0;
 }
